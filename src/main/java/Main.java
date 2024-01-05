b208254e753d881608b893a49864fd1597c801b4
Timothy Bartsch <timothybartsch0929@gmail.com>
2024-01-05 15:36:30 -0800

    Added a method to print a header.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        printHeader();
         printMessage("Hello world!");
         printMessage("By: Timothy");
     }
 
     private static void printMessage(String message){
         System.out.println(message);
+    }
+
+
+    private static void printHeader(){
+        System.out.println("**********************************");
     }
 }
