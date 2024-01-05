b7bc86f83785a19dbabf7c478f385e4057419285
Timothy Bartsch <timothybartsch0929@gmail.com>
2024-01-05 15:39:31 -0800

    Revert "Refactored my Main class."
    This reverts commit a0a7aa93

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        printHeader();
-        printMessage("Hello world!");
-        printMessage("By: Timothy");
-    }
-
-    private static void printMessage(String message){
-        System.out.println(message);
-    }
-
-
-    private static void printHeader(){
-        System.out.println("**********************************");
+        System.out.println("Hello world!");
+        System.out.println("By: Timothy");
     }
 }
